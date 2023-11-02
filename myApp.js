99fb3425d10c5b93a2950f44de75b486c3727b6c
whoisaditya <mitraaditya1357@gmail.com>
2023-11-02 16:24:07 +0530

    fix: Task 6 message style

--- myApp.js
+++ myApp.js
@@ -20,11 +20,12 @@ app.use('/public', express.static(__dirname + '/public'));
 // })  
 
 app.get('/json', (req, res) => {
+    let response = "Hello json"
     if(process.env.MESSAGE_STYLE == "uppercase") {
-        res.json({"message": "HELLO JSON"});
-    } else {
-        res.json({"message": "Hello json"});
-    }
+        response.toUpperCase();
+    } 
+
+    res.send(response)
 })  
 
 module.exports = app;
