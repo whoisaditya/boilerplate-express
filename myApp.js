fd0971728c4fd3a25ec37877ab524f7cd5d15a9d
whoisaditya <mitraaditya1357@gmail.com>
2023-11-02 14:38:43 +0530

    fix: Task 3 file path

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ let app = express();
 //     res.send('Hello Express');
 // })
 
-let absolutePath = __dirname + 'views/index.html'
+let absolutePath = __dirname + '/views/index.html'
 app.get('/', (req, res) => {
     res.send(absolutePath);
 })
