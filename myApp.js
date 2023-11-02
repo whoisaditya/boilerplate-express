e73ced8c1b570ce26ae283ff830b68165b1db7ce
whoisaditya <mitraaditya1357@gmail.com>
2023-11-02 16:37:10 +0530

    fix: Task 6 txt use ===

--- myApp.js
+++ myApp.js
@@ -21,7 +21,7 @@ app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
     let txt = "Hello json"
-    if(process.env.MESSAGE_STYLE == "uppercase") {
+    if(process.env.MESSAGE_STYLE === "uppercase") {
         txt = "HELLO JSON"
     } 
 
