585a9b109e01965c9d07a718a6b5ccb2ae3c3db0
whoisaditya <mitraaditya1357@gmail.com>
2023-11-02 15:46:31 +0530

    feat: Task 6 message style

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 // console.log('Hello World');
 
@@ -14,8 +15,16 @@ app.get('/', (req, res) => {
 
 app.use('/public', express.static(__dirname + '/public'));
 
+// app.get('/json', (req, res) => {
+//     res.json({"message": "Hello json"});
+// })  
+
 app.get('/json', (req, res) => {
-    res.json({"message": "Hello json"});
+    if(process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    }
 })  
 
 module.exports = app;
